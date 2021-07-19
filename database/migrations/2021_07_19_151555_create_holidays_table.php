74776125d7f0acc0a62b4dc0f2abf3b1deb30480
alexellevu <newnapster83@gmail.com>
2021-07-19 18:04:34 +0200

    creata migrazione holidays e inserita nel db

---
+++ database/migrations/2021_07_19_151555_create_holidays_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string('place');
+            $table->string('region');
+            $table->decimal('price');
+            $table->string('hotel');
+            $table->string('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
