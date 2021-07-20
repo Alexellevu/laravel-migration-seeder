bfb28fb8bf73fb150958daf2650a09e3d613043e
alexellevu <newnapster83@gmail.com>
2021-07-20 09:26:08 +0200

    aggiunto css tramite sass

--- resources/views/holidays.blade.php
+++ resources/views/holidays.blade.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout.app')
 
-        <title>Laravel</title>
+@section('title', 'Home Page')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        
-       
-    </head>
-    <body>
+@section ('content')
 
-        
-    @foreach ($holidays as $holiday)
+@foreach ($holidays as $holiday)
     
-           <div class="card">
-                <h2> {{$holiday->city}} </h2>
-                <img src="{{$holiday->image_url}}" >
-                <h3>{{$holiday->country}}</h3>
-                <h4>{{$holiday->price}}</h4>
-          
-            </div>
-    
-    @endforeach
+    <div class="card">
+         <h2> {{$holiday->city}} </h2>
+         <h3>{{$holiday->country}}</h3>
+         <img src="{{$holiday->image_url}}" >
         
+         <h4>price: {{$holiday->price}} $</h4>
+   
+     </div>
 
-    </body>
-</html>
+@endforeach
+
+@endsection
